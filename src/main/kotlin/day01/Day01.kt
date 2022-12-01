0ae9eadec530437136db0551622bf3064835fe57
hampus <hampus.hauffman@wearelevels.com>
2022-12-01 11:37:09 +0100

    day01 done

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -5,11 +5,46 @@ import readInput
 fun main(args: Array<String>) {
 
     fun part1(input: List<String>): Int {
-        return 0
+
+        // An elf is a Mutable list and this is a list of elfs
+        val elfs: MutableList<MutableList<Int>> = mutableListOf()
+
+        elfs.add(mutableListOf());
+
+        input.forEach {
+            // Add an elf if blank line
+            if (it.isBlank()) {
+                elfs.add(mutableListOf());
+            } else {
+                elfs.last().add(it.toInt());
+            }
+
+        }
+
+         elfs.sortBy { it.sum() }
+
+        return elfs.last().sum()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        // An elf is a Mutable list and this is a list of elfs
+        val elfs: MutableList<MutableList<Int>> = mutableListOf()
+
+        elfs.add(mutableListOf());
+
+        input.forEach {
+            // Add an elf if blank line
+            if (it.isBlank()) {
+                elfs.add(mutableListOf());
+            } else {
+                elfs.last().add(it.toInt());
+            }
+
+        }
+
+        elfs.sortBy { -it.sum() }
+
+        return elfs[0].sum()+elfs[1].sum()+elfs[2].sum()
     }
 
     val input = readInput("01")
